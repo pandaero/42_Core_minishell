61deeb3bb9a7f5e151676ba32688bed29bbea45e
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 16:43:15 +0100

    removed freeing of things that don't need freeing

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:20:41 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/17 15:27:46 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:41:01 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ void	error_cmd_nf(char *line, t_minidata *minidata)
 	write(STDERR, cnf, ft_strlen(cnf));
 	write(STDERR, "\n", 1);
 	free(cnf);
-	free(line);
-	free_minidata(minidata);
 }
 
 //Function handles an "no permission cmd" error.
