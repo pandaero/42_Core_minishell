b0fe9a934a54b2305ed2e3a7d175e2d5e5febe7d
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 16:07:32 +0100

    added function to free split-created arrays

--- src/memory.c
+++ src/memory.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 15:17:16 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/17 15:21:45 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:07:08 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@
 //Function frees a minidata struct.
 void	free_minidata(t_minidata *minidata)
 {
+	free_split(minidata->builtincmds);
+}
+
+//Function frees a 2D char array made from ft_split.
+void	free_split(char **charr)
+{
 	int	i;
 
 	i = 0;
-	while (minidata->builtincmds[i])
+	while (charr[i] != 0)
 	{
-		free(minidata->builtincmds[i]);
+		free(charr[i]);
 		i++;
 	}
-	free(minidata->builtincmds[i]);
-	free(minidata->builtincmds);
+	free(charr);
 }
