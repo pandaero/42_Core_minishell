2b99fab6f2bc2e4904b82ec647828deeb2120718
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 16:08:23 +0100

    added function to check whether command is in path

---
+++ src/commands.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   commands.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:34:20 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/11/17 16:07:51 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+//Function determines whether a command is found within the path.
+int	is_pathcmd(char *cmd, char **env)
+{
+	char	*fullpath;
+	char	**splitpath;
+	char	*temp;
+	char	*temp2;
+	int		i;
+
+	while (ft_strncmp(env[i], "PATH=", 5) != 0)
+		i++;
+	fullpath = ft_substr(env[i], 5, ft_strlen(env[i]));
+	splitpath = ft_split(fullpath, ':');
+	i = 0;
+	while (splitpath[i] != 0)
+	{
+		temp = ft_strjoin(splitpath[i], "/");
+		temp2 = ft_strjoin(temp, cmd);
+		free(temp);
+		free(splitpath[i]);
+		splitpath[i] = temp2;
+		if (access(splitpath[i], F_OK) == 0)
+		{
+			if (access(splitpath[i], X_OK) == 0)
+				return (1);
+		}
+		i++;
+	}
+	free_split(splitpath);
+	return (0);
+}
