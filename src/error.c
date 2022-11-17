2ba5c030b3fb004473fce4816f456a424241cc91
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 15:30:30 +0100

    freed some memory before error-induced exit

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:20:41 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/17 15:05:43 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 15:27:46 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 
 //Function handles an "command not found" error.
-void	error_cmd_nf(const char *line)
+void	error_cmd_nf(char *line, t_minidata *minidata)
 {
 	char	*cnf;
 
@@ -24,6 +24,8 @@ void	error_cmd_nf(const char *line)
 	write(STDERR, cnf, ft_strlen(cnf));
 	write(STDERR, "\n", 1);
 	free(cnf);
+	free(line);
+	free_minidata(minidata);
 }
 
 //Function handles an "no permission cmd" error.
