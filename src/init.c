a0867b27160fb75654463b1e78a212ff5f93c539
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 15:15:52 +0100

    added function to initialise minidata struct

---
+++ src/init.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:08:30 by pandalaf          #+#    #+#             */
+/*   Updated: 2022/11/17 15:15:34 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+#include <stdlib.h>
+
+//Function initialises the minidata.
+void	init_minidata(t_minidata *minidata)
+{
+	char	*builtin;
+
+	builtin = ft_strdup("echo.cd.pwd.export.unset.env.exit");
+	minidata->builtincmds = ft_split(builtin, '.');
+}
