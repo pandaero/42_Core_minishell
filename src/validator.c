1ef3cbc5bfaa7e5507da2ebcca2f9e752be92fb2
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 16:44:26 +0100

    wrote the line validator

--- src/validator.c
+++ src/validator.c
@@ -6,16 +6,31 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:43:48 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/16 18:05:17 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:43:54 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 //Function checks that an input line contains valid instructions
-int	validline(const char *str)
+int	validline(const char *line, t_minidata *minidata)
 {
-	if (str)
+	char	*cmd;
+	int		i;
+
+	cmd = findcommand(line);
+	i = 0;
+	while (minidata->builtincmds[i] != 0)
+	{
+		if (ft_strlen(cmd) == ft_strlen(minidata->builtincmds[i]))
+		{
+			if (ft_strncmp(cmd, minidata->builtincmds[i], ft_strlen(cmd)) == 0)
+				return (1);
+		}
+		i++;
+	}
+	i = 0;
+	if (is_pathcmd(cmd, minidata->env) == 1)
 		return (1);
-	return (1);
+	return (0);
 }
