04e6bb7df6bcf320cfe7dfe079cc6abe49975657
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-17 16:44:50 +0100

    refactored

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:53:13 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/16 18:15:33 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:43:35 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,37 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 
-//Program is an interactive shell.
-int	main(int argc, char **argv, char **env)
+static void	main_loop(t_minidata *minidata)
 {
 	char	*line;
 
 	line = (char *) 1;
-	if (argv && env && argc == 1)
+	while (line != 0)
 	{
-		while (line != 0)
+		setup_signal();
+		line = readline("$>");
+		if (validline(line, minidata) == 1)
 		{
-			setup_signal();
-			line = readline("$>");
-			if (validline(line) == 1)
-			{
-				add_history (line);
-				ft_printf("Command line was valid.\n");
-			}
-			else if (validline(line) == -1)
-				error_cmd_nf(line);
-			free(line);
+			add_history (line);
+			ft_printf("Command line was valid.\n");
 		}
+		else
+			error_cmd_nf(line, minidata);
+		free(line);
+	}
+}
+
+//Program is an interactive shell.
+int	main(int argc, char **argv, char **env)
+{
+	t_minidata	*minidata;
+
+	if (argv && argc == 1)
+	{
+		minidata = (t_minidata *)malloc(sizeof(t_minidata));
+		minidata->env = env;
+		init_minidata(minidata);
+		main_loop(minidata);
 	}
 	else
 		write(STDERR, "No arguments are required", 25);
