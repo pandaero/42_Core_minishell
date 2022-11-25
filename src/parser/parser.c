1a5151f9838e7b0a7d559a803e9f5cbfd13f457a
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-25 17:32:01 +0100

    moved parser from main to own file

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:27:25 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/24 15:33:47 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/25 17:22:35 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,27 @@ char	*findcommand(const char *line)
 	cmd = split[0];
 	free(split);
 	return (cmd);
+}
+
+//Function acts when the input command line is a valid one.
+static void	execution(t_minidata *minidata)
+{
+	if (is_builtincmd(minidata) > 0)
+		builtin_execution(minidata);
+	//path (check permissions)
+	//direct (check permissions)
+}
+
+//Function performs the parsing of a command line.
+void	parser(t_minidata *minidata)
+{
+	if (is_valid_quotes(minidata->currline) == 0)
+		ft_printf("syntax error: unable to locate closing quotation\n");
+	if (ft_read_token(minidata) == 0)
+		ft_printf("\n");
+	start_parser(minidata);
+	if (is_validline(minidata) == 1)
+		execution(minidata);
+	else
+		error_cmd_nf(minidata->currline);
 }
