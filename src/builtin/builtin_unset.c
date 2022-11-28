707909dc73e1a19d684467875d2c9b091f475a8a
pandalaf <pandalaf@student.42wolfsburg.de>
2022-11-28 20:29:49 +0100

    completed the unset builtin

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -6,15 +6,25 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 20:24:26 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/24 12:05:04 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/11/28 20:14:58 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+#include <stdlib.h>
 
 //Function clears the contents of a local environment variable.
 void	builtin_unset(t_minidata *minidata)
 {
-	if (minidata)
-		return ;
+	t_envvar	*var;
+	char		**splitline;
+	char		*search;
+
+	splitline = ft_split(minidata->currline, ' ');
+	search = ft_strdup(splitline[1]);
+	free_split(splitline);
+	var = find_env_var_list(minidata, search);
+	if (var)
+		rem_env_var(minidata->envlist, var);
+	free(search);
 }
