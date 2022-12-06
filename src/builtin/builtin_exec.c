13ec229ca85dd29952ea1acd15b00ed955e5c0cf
pandalaf <pandalaf@student.42wolfsburg.de>
2022-12-06 20:01:39 +0100

    changed style of checking for builtin

--- src/builtin/builtin_exec.c
+++ src/builtin/builtin_exec.c
@@ -6,27 +6,27 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 20:08:57 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/11/24 12:05:09 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/12/06 19:05:12 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 //Function handles the execution of built-in commands.
-void	builtin_execution(t_minidata *minidata)
+void	builtin_execution(t_minidata *minidata, int builtin)
 {
-	if (is_builtincmd(minidata) == 1)
+	if (builtin == 1)
 		builtin_exit(minidata);
-	if (is_builtincmd(minidata) == 2)
+	if (builtin == 2)
 		builtin_pwd(minidata);
-	if (is_builtincmd(minidata) == 3)
+	if (builtin == 3)
 		builtin_export(minidata);
-	if (is_builtincmd(minidata) == 4)
+	if (builtin == 4)
 		builtin_unset(minidata);
-	if (is_builtincmd(minidata) == 5)
+	if (builtin == 5)
 		builtin_env(minidata);
-	if (is_builtincmd(minidata) == 6)
+	if (builtin == 6)
 		builtin_cd(minidata);
-	if (is_builtincmd(minidata) == 7)
+	if (builtin == 7)
 		builtin_echo(minidata);
 }
