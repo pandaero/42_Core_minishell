0949e6512a0e4e20c7f3e9ce1440610e583ffed5
Zafarbek Yunusov <zyunusov@c2s14d75.42wolfsburg.de>
2022-12-11 19:49:30 +0100

    now perfect builins

--- src/builtin/builtin_exec.c
+++ src/builtin/builtin_exec.c
@@ -6,7 +6,7 @@
 /*   By: zyunusov <zyunusov@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 20:08:57 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/12/09 22:16:41 by zyunusov         ###   ########.fr       */
+/*   Updated: 2022/12/11 19:44:53 by zyunusov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ int	is_builtincmd(char	*cmd)
 	if (ft_strncmp("exit", cmd, 5) == 0)
 		return (1);
 	if (ft_strncmp("pwd", cmd, 4) == 0)
-		return (2);
-	if (ft_strncmp("export", cmd, 7) == 0)
-		return (3);
-	if (ft_strncmp("unset", cmd, 6) == 0)
-		return (4);
-	if (ft_strncmp("env", cmd, 4) == 0)
 		return (5);
-	if (ft_strncmp("cd", cmd, 3) == 0)
+	if (ft_strncmp("export", cmd, 7) == 0)
+		return (2);
+	if (ft_strncmp("unset", cmd, 6) == 0)
+		return (3);
+	if (ft_strncmp("env", cmd, 4) == 0)
 		return (6);
+	if (ft_strncmp("cd", cmd, 3) == 0)
+		return (4);
 	if (ft_strncmp("echo", cmd, 5) == 0)
 		return (7);
 	return (0);
@@ -37,15 +37,15 @@ void	builtin_execution(t_minidata *minidata, int builtin)
 {
 	if (builtin == 1)
 		builtin_exit(minidata);
-	if (builtin == 2)
-		builtin_pwd(minidata);
-	if (builtin == 3)
-		builtin_export(minidata);
-	if (builtin == 4)
-		builtin_unset(minidata);
 	if (builtin == 5)
-		builtin_env(minidata);
+		builtin_pwd(minidata);
+	if (builtin == 2)
+		builtin_export(minidata);
+	if (builtin == 3)
+		builtin_unset(minidata);
 	if (builtin == 6)
+		builtin_env(minidata);
+	if (builtin == 4)
 		builtin_cd(minidata);
 	if (builtin == 7)
 		builtin_echo(minidata);
