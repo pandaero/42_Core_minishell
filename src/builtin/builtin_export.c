c575c98d05f5496f17b2562a405f69238e230f4f
pandalaf <pandalaf@student.42wolfsburg.de>
2022-12-14 17:00:51 +0100

    added success return to export

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,11 +6,12 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 20:21:14 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/12/05 20:49:00 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/12/14 17:00:28 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+#include <unistd.h>
 #include <stdlib.h>
 
 //Function orders the environment variables in alphabetical order and displays.
@@ -60,4 +61,5 @@ void	builtin_export(t_minidata *minidata)
 	}
 	if (args == 1)
 		display_env_alph(minidata);
+	update_return(minidata, EXIT_SUCCESS);
 }
